01fe3544e58eb65be5a84313fe00411b050a2bc8
gamwe59 <epicgamwe59@gmail.com>
2025-08-26 18:47:25 -0700

    yurijson

--- _generate-yuri.js
+++ _generate-yuri.js
@@ -5,16 +5,8 @@ const path = require("path");
 const dir = path.join(__dirname, "yurifolder");
 const files = fs.readdirSync(dir)
 
-const output = `
-<meta name="color-scheme" content="dark light">
-<noscript>enable js for this experience</noscript>
-<script>
-    /** @type {<Type>(arr: Type[]) => Type} */
-    const random = arr => arr[Math.floor(Math.random() * arr.length)]
-    const pics = ${JSON.stringify(files.map(f => "/yurifolder/" + f))}
-    location.replace(random(pics))
-</script>
-`;
+const output = JSON.stringify(files.map(f => "/yurifolder/" + f))
 
-fs.writeFileSync("yuri.html", output);
+
+fs.writeFileSync("yuri.json", output);
 console.log("✅ yuri.html updated with", files.length, "images.");
