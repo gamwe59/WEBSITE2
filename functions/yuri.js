b606d2562a55bf4388ef5c16e6511e1b24320183
gamwe59 <epicgamwe59@gmail.com>
2025-08-26 18:14:17 -0700

    yuri2

--- functions/yuri.js
+++
@@ -1,8 +0,0 @@
-/** @type {<Type>(arr: Type[]) => Type} */
-const random = arr => arr[Math.floor(Math.random() * arr.length)]
-const pictures = [
-  "/yurifolder/1.jpg"
-]
-export function onRequest (context) {
-  return Response.redirect(random(pictures), 307);
-}
