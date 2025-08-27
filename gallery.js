921ff94be0503d60afeac61c9d61cd4623f63c51
gamwe59 <epicgamwe59@gmail.com>
2025-08-27 16:56:34 -0700

    test urlparams

---
+++ gallery.js
@@ -0,0 +1,25 @@
+import yuri from "/yuri.json" with { type: "json" }
+
+let gallery = {}
+let curLoadedFromGallery = 0
+
+const validParams = {sort: "date"}
+const searchParams = new URLSearchParams(params)
+
+function sort() {
+
+}
+
+function resetFilters() {
+    gallery = yuri.slice()
+    gallery.splice(0,1)
+    for (const [key, val] of Object.entries(gallery)) {
+        console.log(val.name)
+    }
+    for (const [key, val] of Object.entries(yuri)) {
+        console.log(val.name)
+    }
+}
+
+
+resetFilters()
