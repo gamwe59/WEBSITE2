f997713800b4a88688a75602b8d2cd1100218644
gamwe59 <epicgamwe59@gmail.com>
2025-08-30 22:48:47 -0700

    fixed main menu

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ import yuri from "/yuri.json" with { type: "json" }
 function randImg() {
   let size = yuri.length
   let x = Math.floor(size * Math.random())
-  document.getElementById('txt').textContent=yuri[x].name.toString()
+  var str = yuri[x].name;
+  str = str.replace(/\s+/g, '-').toLowerCase();
+  document.getElementById('yurisrc').href="./gallery/view?img="+str
   document.getElementById('yuri').src = yuri[x].src;
 }
 
