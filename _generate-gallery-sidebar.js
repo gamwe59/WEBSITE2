f414cd91438ec8349cb7bf68e02943ac05f15d11
gamwe59 <epicgamwe59@gmail.com>
2025-09-07 14:54:16 -0700

    tags sorted by name

--- _generate-gallery-sidebar.js
+++ _generate-gallery-sidebar.js
@@ -6,18 +6,43 @@ const sidebar = document.getElementById("sidepanel")
 let types = {}
 let tagCount = {}
 
+function insertionSort(arr) {
+    let n = arr.length;
+    for (let i = 1; i < n; i++) {
+        let obj = arr[i]
+        let key = obj.long
+        let j = i-1;
+        while (j >= 0 && arr[j].long.toString().localeCompare(key.toString())>=1) {
+            arr[j+1]=arr[j]
+            j--;
+        }
+        arr[j+1]=obj;
+    }
+    return arr;
+}
+
 function addContent() {
     for (const [key, tag] of Object.entries(tags)) {
-        types[tag.type] = {name: tag.type}
+        if (types[tag.type]) {
+            if (!types[tag.type].includes(tag)) {
+                tag.id = key
+                types[tag.type].push(tag)
+            }
+        } else {
+            tag.id = key
+            types[tag.type] = new Array()
+            types[tag.type].push(tag)
+        }
     }
     
     for (const [key, type] of Object.entries(types)) {
         let div = document.createElement("ul")
-        div.id = type.name
+        div.id = key
         let title = document.createElement("header")
-        title.textContent = type.name
+        title.textContent = key
         sidebar.appendChild(div)
         div.appendChild(title)
+        insertionSort(type)
     }
     for (const [key, img] of Object.entries(yuri)) {
         for (const [key2, tag] of Object.entries(img.tags)) {
@@ -28,20 +53,22 @@ function addContent() {
             }
         }
     }
-    for (const [key, tag] of Object.entries(tags)) {
-        let li = document.createElement("li")
-        let b = document.createElement("button")
-        let folder = document.getElementById(tag.type)
-        let count = tagCount[key]
-        if (count) {
-            b.textContent = tag.long+" ["+count+"]"
-        } else {
-            b.textContent = tag.long+" [0]"
+    for (const [key, type] of Object.entries(types)) {
+        for (const [key2, tag] of Object.entries(type)) {
+            let li = document.createElement("li")
+            let b = document.createElement("button")
+            let folder = document.getElementById(tag.type)
+            let count = tagCount[tag.id]
+            if (count) {
+                b.textContent = tag.long+" ["+count+"]"
+            } else {
+                b.textContent = tag.long+" [0]"
+            }
+            b.id = "tagbutton"
+            b.class=tag.id
+            li.appendChild(b)
+            folder.appendChild(li)
         }
-        b.id = "tagbutton"
-        b.class=key
-        li.appendChild(b)
-        folder.appendChild(li)
     }
 }
 
