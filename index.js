95129bad7a1c7e6d7467ea01bdacad378ad9f734
gamwe59 <epicgamwe59@gmail.com>
2025-09-08 16:56:38 -0700

    acessibility

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ import yuri from "/yuri.json" with { type: "json" }
 function randImg() {
   let size = yuri.length
   let x = Math.floor(size * Math.random())
-  var str = yuri[x].name;
-  str = str.replace(/\s+/g, '-').toLowerCase();
-  document.getElementById('yurisrc').href="./gallery/view?img="+str
+  document.getElementById('yurisrc').href="./gallery/view?img="+yuri[x].id
+  document.getElementById('yuri').alt = yuri[x].name
   document.getElementById('yuri').src = yuri[x].src;
 }
 
