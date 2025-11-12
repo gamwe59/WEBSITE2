eea46a30d5a4722f793f66bbc0085112386b8838
gamwe59 <epicgamwe59@gmail.com>
2025-11-11 18:47:31 -0800

    nicole test

--- _functions/gallery/view.js
+++ _functions/gallery/view.js
@@ -1,19 +1,40 @@
-import yuri from "/yuri.json" with { type: "json" }
-
-export async function onRequestGet(ctx) {
-    const response = await ctx.env.ASSETS.fetch(ctx.url)
-    let img = yuri.find(i => i.id.toString() === ctx.url.searchParams.get("img"))
+export async function onRequest(ctx) {
+    const url = new URL(ctx.request.url);
     
-    if (img) {
-        const page = (await response.text())
-            .replaceAll("EMBED ERROR", img.name)
-            .replaceAll("/images/embederror.png", img.src)
-        
-        return new Response(page, {
-            status: response.status,
-            headers: response.headers
-        })
+    if (!url.pathname.includes('/view')) {
+        return ctx.env.ASSETS.fetch(ctx.request);
     }
-
-    return response
+    
+    const imgId = url.searchParams.get("img");
+    
+    if (!imgId) {
+        return ctx.env.ASSETS.fetch(ctx.request);
+    }
+    
+    const yuriJsonResponse = await ctx.env.ASSETS.fetch(new URL('/yuri.json', url.origin));
+    const yuri = await yuriJsonResponse.json();
+    
+    const img = yuri.find(i => i.id.toString() === imgId);
+    
+    if (!img) {
+        return ctx.env.ASSETS.fetch(ctx.request);
+    }
+    
+    const response = await ctx.env.ASSETS.fetch(ctx.request);
+    let html = await response.text();
+    
+    const isVideo = img.tags && img.tags.includes("video");
+    const embedImage = isVideo && img.thumbnail ? img.thumbnail : img.src;
+    
+    html = html
+        .replace('content="EMBED ERROR"', `content="${img.name}"`)
+        .replace('content="/images/embederror.png"', `content="${embedImage}"`);
+    
+    return new Response(html, {
+        status: response.status,
+        headers: {
+            ...Object.fromEntries(response.headers),
+            'Content-Type': 'text/html;charset=UTF-8'
+        }
+    });
 }
