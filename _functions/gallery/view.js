79f876185c874a880cdde5c1137fb08735a22fcd
gamwe59 <epicgamwe59@gmail.com>
2025-11-11 18:35:07 -0800

    nicole test

--- _functions/gallery/view.js
+++ _functions/gallery/view.js
@@ -1,10 +1,19 @@
 import yuri from "/yuri.json" with { type: "json" }
 
 export async function onRequestGet(ctx) {
-	const response = await env.ASSETS.fetch(ctx.url)
+    const response = await ctx.env.ASSETS.fetch(ctx.url)
     let img = yuri.find(i => i.id.toString() === ctx.url.searchParams.get("img"))
+    
     if (img) {
-        const page = await response.text().replaceAll("EMBED ERROR", img.name).replaceAll("/images/embederror.png", img.src)
-        return new Response(page, response)
+        const page = (await response.text())
+            .replaceAll("EMBED ERROR", img.name)
+            .replaceAll("/images/embederror.png", img.src)
+        
+        return new Response(page, {
+            status: response.status,
+            headers: response.headers
+        })
     }
+
+    return response
 }
