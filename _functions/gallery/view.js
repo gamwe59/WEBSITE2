b92b60cf0f371f0738a4768cf69c6c89a00bd6b8
gamwe59 <epicgamwe59@gmail.com>
2025-11-11 18:28:53 -0800

    sigh

--- _functions/gallery/view.js
+++ _functions/gallery/view.js
@@ -4,8 +4,7 @@ export async function onRequestGet(ctx) {
 	const response = await env.ASSETS.fetch(ctx.url)
     let img = yuri.find(i => i.id.toString() === ctx.url.searchParams.get("img"))
     if (img) {
-        const page = 
-            await response.text().replaceAll("EMBED ERROR", img.name).replaceAll("/images/embederror.png", img.src)
+        const page = await response.text().replaceAll("EMBED ERROR", img.name).replaceAll("/images/embederror.png", img.src)
         return new Response(page, response)
     }
 }
