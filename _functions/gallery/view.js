dc976d59f3998d992e43c6a72dcb3d26953e3add
gamwe59 <epicgamwe59@gmail.com>
2025-11-11 18:26:47 -0800

    im killing you

--- _functions/gallery/view.js
+++ _functions/gallery/view.js
@@ -2,7 +2,7 @@ import yuri from "/yuri.json" with { type: "json" }
 
 export async function onRequestGet(ctx) {
 	const response = await env.ASSETS.fetch(ctx.url)
-    let img = yuri.find(i => i.id.toString() === ctx.url.searchparams.get("img"))
+    let img = yuri.find(i => i.id.toString() === ctx.url.searchParams.get("img"))
     if (img) {
         const page = 
             await response.text()
