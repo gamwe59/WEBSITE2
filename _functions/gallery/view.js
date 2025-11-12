883ed6291cb1347b2164bb7ffa007ec16242132d
gamwe59 <epicgamwe59@gmail.com>
2025-11-11 18:11:28 -0800

    test embed

--- _functions/gallery/view.js
+++ _functions/gallery/view.js
@@ -1,7 +1,20 @@
+import yuri from "/yuri.json" with { type: "json" }
+
 export async function onRequestGet(ctx) {
 	const response = await env.ASSETS.fetch(ctx.url)
-	const page = 
-		await response.text()
-			.replaceAll("some template string", "value")
-	return new Response(page, response)
+    let img
+    for (let i = 0; i < n; i++) {
+        if (yuri[i].id.toString() == USP.get("img")) {
+            valid = true
+            img = yuri[i]
+            break;
+        }
+    }
+    if (img) {
+        const page = 
+            await response.text()
+                .replaceAll("EMBED ERROR", img.name)
+                .replaceAll("/images/embederror.png", img.src)
+        return new Response(page, response)
+    }
 }
